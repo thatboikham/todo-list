cc0e15ae7bc96b3c48ed10965c8317564f8e8773
thatboikham <khamjapher1@gmail.com>
2024-03-04 23:12:38 +0300

    feat: add a function that change task status

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import { compareAsc, format } from "date-fns";
 
-function TasktoDO(title, priority, description, dueDate, notes){
+function TasktoDO(title, priority, description, dueDate, notes,taskStatus){
     return{
         title,
         description,
         priority,
         dueDate,
-        notes
+        notes,
+        taskStatus:taskStatus || "incomplete",
     }
 }
 
@@ -14,6 +15,9 @@ function project(name){
     const task = []
     const addTask = (title,description,priority,dueDate,notes) => {
         task.push(TasktoDO(title,priority,description,dueDate,notes));
+    }
+    const changeStatus = () => {
+
     }
     return{
         name,
@@ -27,4 +31,11 @@ function createNewTask(projectObj,title,priority,description,dueDate,notes){
     projectObj.addTask(title,priority,description,dueDate,notes);
 }
 createNewTask(project1,"coding","high","working with factory functions",`${format(new Date(2014, 1, 11), "yyyy-MM-dd")}`,"factory functions are the best")
+createNewTask(project1,"coding","high","working with factory functions",`${format(new Date(2014, 1, 11), "yyyy-MM-dd")}`,"factory functions are the best")
+
 console.log(project1)
+
+function changingTaskStatus(index){
+    project1.task[index].taskStatus = "complete";
+    console.log(project1.task)
+};
